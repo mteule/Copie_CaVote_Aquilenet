d1555d65b3fdc6081052ee55959e1c41393d19e2
mteule <mathias.te@googlemail.com>
2014-12-01 23:03:55 +0100

    Rajoute un script pour générer une installation de développement.

---
+++ install_dev.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# -*- coding: utf-8 -*-
+
+# Installation de Cavote (développement)
+# --------------------------------------
+
+# Récupérer la dernière version de développement :
+#    (L'option GIT_SSL_NO_VERIFY=true n'est pas nécessaire si le certificat 
+#     autosigné a été ajouté de manière permanente au système)
+  GIT_SSL_NO_VERIFY=true git clone https://code.ffdn.org/ffdn/cavote.git
+
+# Installation du virtualenv (requiert le package python-virtualenv)
+  cd cavote
+  virtualenv flask
+  flask/bin/pip install -r requirements.txt
+
+# Installation de la base de données :
+  SCHEMA_SQL=`cat schema.sql schema_0.1.X_to_0.2.X.sql schema_0.2.X_to_0.3.X.sql`
+  sqlite3 --init  /tmp/cavote.db "$SCHEMA_SQL"
+
+# Modifiez les paramètres de settings.py pour qu'ils correspondent à votre installation
+#   nano settings.py.example
+
+# Renommer le fichier settings final
+  mv settings.py.example settings.py
+
+# Activer le virtualenv
+  source flask/bin/activate
+
+# Lancez le serveur en mode développement :
+  ./main.py # & # Remettre le '&' pour lancer directement firefox 
+
+# L'utilisateur par défaut peut être activé à l'adresse suivante:
+#     (Modifiez en les identifiants de connection lors du premier accès.)
+#   firefox http://localhost:5000/login/1/victory 
+
+# Quand on a terminé, désactiver le virtualenv:
+  deactivate
+
